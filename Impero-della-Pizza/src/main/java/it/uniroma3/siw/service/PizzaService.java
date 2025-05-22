952ecc422ad88d9c38fa28dcdd60ca8f6e55b630
DrTechnocaos <92891372+Gabbosenpai@users.noreply.github.com>
2025-05-22 16:41:27 +0200

    pizza repository + service

---
+++ Impero-della-Pizza/src/main/java/it/uniroma3/siw/service/PizzaService.java
@@ -0,0 +1,24 @@
+package it.uniroma3.siw.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.model.Pizza;
+import it.uniroma3.siw.repository.PizzaRepository;
+
+@Service
+public class PizzaService {
+
+	@Autowired
+	private PizzaRepository pizzaRepository;
+	
+	public Pizza getPizzabyId(Long id) {
+		return pizzaRepository.findById(id).get();
+	}
+	
+	public Iterable<Pizza> getAllPizzas() {
+		return pizzaRepository.findAll();
+	}
+}
