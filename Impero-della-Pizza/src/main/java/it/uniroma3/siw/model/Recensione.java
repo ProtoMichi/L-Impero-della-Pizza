1df731cf7ec6cd30c6bdcd6f85789c9477dadcb9
Edenieson <nicsondyhawk@gmail.com>
2025-05-22 16:27:08 +0200

    Aggiunta notazione in Recensione
    Aggiunta notazuone in Recensione

--- Impero-della-Pizza/src/main/java/it/uniroma3/siw/model/Recensione.java
+++ Impero-della-Pizza/src/main/java/it/uniroma3/siw/model/Recensione.java
@@ -2,8 +2,16 @@ package it.uniroma3.siw.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Recensione {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String codice;
 	private String descrizione;
 	private LocalDate creazione;
