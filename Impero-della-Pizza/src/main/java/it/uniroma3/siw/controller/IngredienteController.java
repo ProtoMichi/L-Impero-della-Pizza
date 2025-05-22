cd170a84775b27750edd6a6d6574d2a5d7680158
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-05-22 17:02:14 +0200

    Aggiunto Controller Ingrediente

---
+++ Impero-della-Pizza/src/main/java/it/uniroma3/siw/controller/IngredienteController.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import it.uniroma3.siw.service.IngredienteService;
+
+@Controller
+public class IngredienteController {
+
+	@Autowired
+	private IngredienteService ingredienteService;
+	
+	@GetMapping("/ingrediente")
+	public String getIngredienti(Model model) {
+		model.addAttribute("ingrediente",this.ingredienteService.getAllIngredienti());
+		return "ingredienti.html";
+	}
+	
+	@GetMapping("/ingrediente/{id}")
+	public String getIngrediente(@PathVariable("id") Long id,Model model) {
+		model.addAttribute("ingrediente",this.ingredienteService.getIngredientebyId(id));
+		return "ingrediente.html";
+	}
+}
