5ecf884c1fde451b72ab9da7503da18bda75e319
Edenieson <nicsondyhawk@gmail.com>
2025-05-22 17:38:58 +0200

    rece service
    rece service

---
+++ Impero-della-Pizza/src/main/java/it/uniroma3/siw/service/RecensioneService.java
@@ -0,0 +1,22 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.model.Recensione;
+import it.uniroma3.siw.repository.RecensioneRepository;
+
+@Service
+public class RecensioneService {
+	
+	@Autowired
+	RecensioneRepository recensioneRepository;
+	
+	public Recensione getRecensioneById(Long id) {
+		return recensioneRepository.findById(id).get();		
+	}
+	
+	public Iterable<Recensione> getAllRecensioni(){
+		return recensioneRepository.findAll();
+	}
+}
