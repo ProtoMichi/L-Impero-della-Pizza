f7d365e6a0069baf0f47270a7566880da0ef3e50
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-05-22 18:24:48 +0200

    Aggiornato import

--- Impero-della-Pizza/target/classes/import.sql
+++ Impero-della-Pizza/target/classes/import.sql
@@ -1,13 +1,14 @@
 ALTER TABLE pizza ALTER COLUMN id SET DEFAULT nextval('pizza_seq');
+ALTER TABLE ingrediente ALTER COLUMN id SET DEFAULT nextval('ingrediente_seq');
 
 INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Margherita', 5.0, '/images/pizza-margherita.jpg');
 INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Diavola', 7.0, '/images/pizza-diavola.jpg');	
 INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Quattro Formaggi', 8.0,'/images/pizza-quattro-formaggi.jpg');
 
-INSERT INTO ingrediente(id,nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Pomodoro',1.50,true,true);
-INSERT INTO ingrediente(id,nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Mozzarella',2.50,true,false);
-INSERT INTO ingrediente(id,nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Basilico',0.50,true,true);
-INSERT INTO ingrediente(id,nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Farina',1.50,false,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Pomodoro',1.50,true,true);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Mozzarella',2.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Basilico',0.50,true,true);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES(nextval('ingrediente_seq'),'Farina',1.50,true,false);
 
 
 
