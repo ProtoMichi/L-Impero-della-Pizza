ac8f94db4e146b18f636b520ae43599cf2ac013b
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-05-24 17:05:29 +0200

    Aggiornato codice java Ingrediente

--- Impero-della-Pizza/src/main/java/it/uniroma3/siw/model/Ingrediente.java
+++ Impero-della-Pizza/src/main/java/it/uniroma3/siw/model/Ingrediente.java
@@ -1,59 +1,76 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Ingrediente {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-	
+	private Long id;
+
+	@Column(nullable=false)
 	private String nome;
-	private Double prezzo;
+
+	@Column(nullable=false)
+	private Float prezzo;
+
+	@Column(nullable=false)
 	private Boolean vegan;
+
+	@Column(nullable=false)
 	private Boolean celiaco;
-	
-//	public Ingrediente(String nome,Float prezzo,Boolean vegan,Boolean celiaco) {
-//		this.nome = nome;
-//		this.prezzo = prezzo;
-//		this.vegan = vegan;
-//		this.celiaco = celiaco;
-//	}
-	
+
+	@ManyToMany
+	private List<Pizza> pizze;
+
+	public Ingrediente() {
+
+	}
+
+	public Ingrediente(String nome,Float prezzo,Boolean vegan,Boolean celiaco) {
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.vegan = vegan;
+		this.celiaco = celiaco;
+	}
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public Double getPrezzo() {
+
+	public Float getPrezzo() {
 		return prezzo;
 	}
-	
-	public void setPrezzo(Double prezzo) {
+
+	public void setPrezzo(Float prezzo) {
 		this.prezzo = prezzo;
 	}
-	
+
 	public Boolean getVegan() {
 		return this.vegan;
 	}
-	
+
 	public void setVegan(Boolean vegan) {
 		this.vegan = vegan;
 	}
-	
+
 	public Boolean getCeliaco() {
 		return this.celiaco;
 	}
-	
+
 	public void setCiliaco(Boolean celiaco) {
 		this.celiaco = celiaco;
 	}
