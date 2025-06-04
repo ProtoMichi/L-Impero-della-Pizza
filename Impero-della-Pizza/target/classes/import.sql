f49182d518891407a6cb45209f5b29b179130497
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-06-04 18:33:14 +0200

    Aggiustamenti Ingredienti

--- Impero-della-Pizza/target/classes/import.sql
+++ Impero-della-Pizza/target/classes/import.sql
@@ -7,9 +7,10 @@ INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Diavola', 7.00, '/images/
 INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Quattro Formaggi', 8.00,'/images/pizza-quattro-formaggi.jpg');
 
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Pomodoro',1.50,true,true);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Mozzarella',2.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Mozzarella',2.50,false,true);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Basilico',0.50,true,true);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina',1.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Cheddar',1.50,false,false);
 
 INSERT INTO recensione(pizza_id, descrizione, data_di_creazione, stelle) VALUES (51,'DIAVOLA SUPER BUONISSIMA!', '03-10-2023', 5.0);
 INSERT INTO recensione(pizza_id, descrizione, data_di_creazione, stelle) VALUES (1,'Ottime pizze, personale gentile, ma i baffi del titolare puzzano', '16-07-2024', 4.0);
