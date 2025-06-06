cf5d0aeb594ad4b737b6108b1e8ef47e0c7219e8
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-06-06 15:36:27 +0200

    Aggiornato Import

--- Impero-della-Pizza/src/main/resources/import.sql
+++ Impero-della-Pizza/src/main/resources/import.sql
@@ -2,15 +2,25 @@ ALTER TABLE pizza ALTER COLUMN id SET DEFAULT nextval('pizza_seq');
 ALTER TABLE ingrediente ALTER COLUMN id SET DEFAULT nextval('ingrediente_seq');
 ALTER TABLE recensione ALTER COLUMN id SET DEFAULT nextval('recensione_seq');
 
-INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Margherita', 5.00, '/images/pizza-margherita.jpg');
-INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Diavola', 7.00, '/images/pizza-diavola.jpg');	
-INSERT INTO pizza (nome, prezzo, URLImmagine) VALUES ('Quattro Formaggi', 8.00,'/images/pizza-quattro-formaggi.jpg');
 
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Pomodoro',1.50,true,true);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Mozzarella',2.50,false,true);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Basilico',0.50,true,true);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina',1.50,true,false);
 INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Cheddar',1.50,false,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina 00',1.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina 0',1.00,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina manitoba',2.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina integrale',3.00,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di semola rimacinata di grano duro',3.50,true,false);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di riso',3.50,true,true);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di mais',2.75,true,true);
+INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di grano saraceno',4.50,true,true);
+
+
+INSERT INTO pizza (nome,prezzo, URLImmagine,farina_id) VALUES ('Margherita',5.00, '/images/pizza-margherita.jpg',301);
+INSERT INTO pizza (nome,prezzo, URLImmagine,farina_id) VALUES ('Diavola',7.00, '/images/pizza-diavola.jpg',251);	
+INSERT INTO pizza (nome,prezzo, URLImmagine,farina_id) VALUES ('Quattro Formaggi',8.00,'/images/pizza-quattro-formaggi.jpg',151);
 
 INSERT INTO pizza_lista_ingredienti(lista_pizze_id,lista_ingredienti_id) VALUES (1,1);
 INSERT INTO pizza_lista_ingredienti(lista_pizze_id,lista_ingredienti_id) VALUES (1,51);
@@ -25,13 +35,3 @@ INSERT INTO users(id, nome, cognome, email) VALUES(nextval('users_seq'), 'paolo'
 INSERT INTO credentials(id, password, ruolo, username, user_id) VALUES(nextval('credentials_seq'), '$2a$10$F605H0XOC06ODyI.oQnCzeMWpWRPNOH2DM2Lmf.ZSAkNrTG6Kqa3q', 'DEFAULT', 'paolo',currval('users_seq'));
 INSERT INTO users(id, nome, cognome, email) VALUES(nextval('users_seq'), 'admin', 'admin', 'admin@admin.com');
 INSERT INTO credentials(id, password, ruolo, username, user_id) VALUES(nextval('credentials_seq'), '$2a$12$Lzmhtr/JjTVh8zJF.6h6IOswJfrt.zmuS3yjvjZW999TJdDoRroQu', 'ADMIN', 'admin',currval('users_seq'));
-
-
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina 00',1.50,true,false);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina 0',1.00,true,false);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina manitoba',2.50,true,false);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina integrale',3.00,true,false);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di semola rimacinata di grano duro',3.50,true,false);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di riso',3.50,true,true);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di mais',2.75,true,true);
-INSERT INTO ingrediente(nome,prezzo,vegan,celiaco)VALUES('Farina di grano saraceno',4.50,true,true);
